872dc314a813be2bbd72a56130368f89d00dfba8
Kedino <kedino@gmail.com>
2025-05-14 09:44:44 +0200

    Draw player

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player
+
 #import os
 
 #print(f"Current working directory: {os.getcwd()}")
@@ -11,12 +14,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
         
 
 if __name__ == "__main__":
